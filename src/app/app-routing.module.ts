ec14d842c9fdcdb957690f774e9788e55adef960
Ollebacx <dav.ollebac@gmail.com>
2020-07-17 12:07:52 +0100

    In & out guards working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,18 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { SignComponent } from './pages/sign/sign.component';
 import { LoginComponent } from './pages/login/login.component';
 import { OtherprofileComponent } from './pages/otherprofile/otherprofile.component';
+import { AuthGuard } from './guards/auth.guard';
+import { QuickLogGuard } from './guards/quick-log.guard';
 
 
 const routes: Routes = [
 
-  { path: '', redirectTo: '/sign', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'sign', component: SignComponent },
-  { path: 'sign/up', component: SignupComponent },
-  { path: 'login', component: LoginComponent},
-  { path: 'home/:username', component: OtherprofileComponent}
+  { path: '', redirectTo: '/sign', pathMatch: 'full', canActivate: [QuickLogGuard] },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'sign', component: SignComponent, canActivate: [QuickLogGuard] },
+  { path: 'sign/up', component: SignupComponent, canActivate: [QuickLogGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [QuickLogGuard]},
+  { path: 'home/:username', component: OtherprofileComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
